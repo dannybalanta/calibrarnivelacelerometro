269433be213b5c3e74d0c4685ea41c2a491aca3e
dannybalanta <42857316+dannybalanta@users.noreply.github.com>
2020-11-09 20:43:56 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,32 @@
+basic.forever(function () {
+    if (input.acceleration(Dimension.X) >= -5 && input.acceleration(Dimension.X) <= 5) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            # # # # #
+            . . . . .
+            . . . . .
+            `)
+        basic.clearScreen()
+        basic.pause(500)
+        basic.showString("OK")
+        basic.clearScreen()
+        basic.pause(500)
+    } else if (input.acceleration(Dimension.X) > 5) {
+        basic.showLeds(`
+            . . . . #
+            . . . # .
+            . . # . .
+            . # . . .
+            # . . . .
+            `)
+    } else if (input.acceleration(Dimension.X) < -5) {
+        basic.showLeds(`
+            # . . . .
+            . # . . .
+            . . # . .
+            . . . # .
+            . . . . #
+            `)
+    }
+})
